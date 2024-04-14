2fbdef5bf9c5e106e834d1d5ae5847c3ae0c8785
AtinadCodes <lamptey.danita@gmail.com>
2024-04-14 15:05:54 +0000

    changes to Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,28 +15,15 @@ function Login() {
         <div className="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden">
           <div
             className="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center"
-            style={{ backgroundImage: "url('https://images.unsplash.com/photo-1709534486708-fb8f94150d0a?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}
+            style={{ backgroundImage: "url('https://images.unsplash.com/photo-1707157281599-d155d1da5b4c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZXhwZW5zZSUyMHRyYWNrZXJ8ZW58MHx8MHx8fDA%3D')" }}
           >
           
           </div>
           <div className="w-full lg:w-1/2 py-16 px-12">
-            <h2 className="text-3xl mb-4">Register</h2>
-            <p className="mb-4 text-cyan-500">
-              Create your account. It’s free and only takes a minute
-            </p>
+            <h2 className="text-3xl mb-4">Sign In</h2>
+           
             <form action="#">
-              <div className="grid grid-cols-2 gap-5">
-                <input
-                  type="text"
-                  placeholder="Firstname"
-                  className="border border-gray-400 py-1 px-2"
-                />
-                <input
-                  type="text"
-                  placeholder="Surname"
-                  className="border border-gray-400 py-1 px-2"
-                />
-              </div>
+              
               <div className="mt-5">
                 <input
                   type="text"
@@ -51,29 +38,21 @@ function Login() {
                   className="border border-gray-400 py-1 px-2 w-full"
                 />
               </div>
+              
               <div className="mt-5">
-                <input
-                  type="password"
-                  placeholder="Confirm Password"
-                  className="border border-gray-400 py-1 px-2 w-full"
-                />
-              </div>
-              <div className="mt-5">
-                <input type="checkbox" className="border border-gray-400" />
+               
                 <span>
                   {" "}
                   <a href="#" className="text-cyan-500 font-semibold">
-                    I Accept Terms of Use
+                    Forgot your Password?
                   </a>{" "}
                   &{" "}
-                  <a href="#" className="text-cyan-500 font-semibold">
-                    Sign In Here
-                  </a>
+                  
                 </span>
               </div>
               <div className="mt-5">
                 <button className="w-full bg-cyan-500 py-3 text-center text-white">
-                  Register Now
+                  Login In
                 </button>
               </div>
             </form>
